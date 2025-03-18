126d2541320b77227bb319461ca08c1bdfde9bce
chunchunmaru12 <princekhan456.pk10@gmail.com>
2025-03-18 08:24:29 +0545

    Calculator

--- ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorArea.java
+++ ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorArea.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,9 +18,11 @@ public class CalculatorArea extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_calculator_area, container, false);
-
+        Bundle bundle = getArguments();
+        String area = bundle.getString("area");
+        TextView resultText = view.findViewById(R.id.resArea);
+        resultText.setText("Area: " + area);
         return view;
     }
 }
--- ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorInput.java
+++ ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorInput.java
@@ -3,6 +3,7 @@ package com.example.chapterfive;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,18 +20,37 @@ public class CalculatorInput extends Fragment {
         View view= inflater.inflate(R.layout.fragment_calculator_input, container, false);
         EditText length=view.findViewById(R.id.textLength);
         EditText breadth=view.findViewById(R.id.textBreadth);
+
+
         Button calcArea=view.findViewById(R.id.calcArea);
         Button calcPeri=view.findViewById(R.id.calcPeri);
+
+
         calcArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                float lengthC = Float.parseFloat(length.getText().toString());
+                float breadthC = Float.parseFloat(breadth.getText().toString());
+                float area = lengthC * breadthC;
+                bundle.putString("area", String.valueOf(area));
+                CalculatorArea areaFragment = new CalculatorArea();
+                areaFragment.setArguments(bundle);
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentCalculator,areaFragment).commit();
 
             }
         });
         calcPeri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Bundle bundle = new Bundle();
+                float lengthC = Float.parseFloat(length.getText().toString());
+                float breadthC = Float.parseFloat(breadth.getText().toString());
+                float perimeter = 2*(lengthC+breadthC);
+                bundle.putString("perimeter", String.valueOf(perimeter));
+                CalculatorPerimeter periFragment= new CalculatorPerimeter();
+                periFragment.setArguments(bundle);
+                getParentFragmentManager().beginTransaction().replace(R.id.fragmentCalculator,periFragment).commit();
             }
         });
 
