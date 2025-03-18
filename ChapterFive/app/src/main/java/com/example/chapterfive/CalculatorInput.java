42af9f11090c04db20cc8836680dc2bf2dd581a8
chunchunmaru12 <princekhan456.pk10@gmail.com>
2025-03-18 07:43:24 +0545

    Calculator

--- ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorInput.java
+++ ChapterFive/app/src/main/java/com/example/chapterfive/CalculatorInput.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalculatorInput#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CalculatorInput extends Fragment {
 
     @Override
@@ -24,8 +19,15 @@ public class CalculatorInput extends Fragment {
         View view= inflater.inflate(R.layout.fragment_calculator_input, container, false);
         EditText length=view.findViewById(R.id.textLength);
         EditText breadth=view.findViewById(R.id.textBreadth);
-        Button calc=view.findViewById(R.id.calcResult);
-        calc.setOnClickListener(new View.OnClickListener() {
+        Button calcArea=view.findViewById(R.id.calcArea);
+        Button calcPeri=view.findViewById(R.id.calcPeri);
+        calcArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        calcPeri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
